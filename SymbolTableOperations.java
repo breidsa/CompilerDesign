4ee3dd7a409354c7900cfbdc0b7061568c68bae7
breidsa <77807959+breidsa@users.noreply.github.com>
2022-04-07 09:16:03 +0100

    Update SymbolTableOperations.java

--- SymbolTableOperations.java
+++ SymbolTableOperations.java
@@ -1,4 +1,20 @@
+package symbol_table;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+
 class symbolMethods {
+    
+    
+    private Stack<Scope> indexStructure;
+
     public static void main(String args[]) {
 
     }
