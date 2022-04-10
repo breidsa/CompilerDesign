73b45b151290628233c8c44dd10d600cdba9ff52
breidsa <77807959+breidsa@users.noreply.github.com>
2022-04-10 11:22:59 +0100

    Update SymbolTableOperations.java

--- SymbolTableOperations.java
+++ SymbolTableOperations.java
@@ -1,12 +1,46 @@
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
+
+
+
+public class Scope {
+    
+     private Hashtable<String,Integer> hash;
+     private int parseLevel;
+     
+     public Scope(int parseLevel){
+        this.parseLevel = parseLevel;
+        hash = new Hashtable<>();
+     }
+     
+     public Scope(){
+        parseLevel = 0;
+     }
+
+     public void define(String name, Integer val){
+        hash.put(name, val);
+     }
+
+     public boolean isMatching(String name){
+        return hash.containsKey(name);
+     }
+
+     public Integer getVal(String name){
+        return  hash.get(name);
+     }
+    
+     public int getParseLevel(){
+        return parseLevel;
+     }
+    }
+
+    
+    
+    public class SymbolTable {
+    enter_scope()
+    exit_scope()
+    
+    }
 
 class symbolMethods {
 
