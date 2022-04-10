feb272144f2d790f23192363d16760f0e7526d64
breidsa <77807959+breidsa@users.noreply.github.com>
2022-04-10 11:57:22 +0100

    Update SymbolTableOperations.java

--- SymbolTableOperations.java
+++ SymbolTableOperations.java
@@ -6,18 +6,18 @@ import java.util.*;
 public class Scope {
     
      private Hashtable<String,Integer> hash;
-     private int parseLevel;
+     private int parseLvl;
      
-     public Scope(int parseLevel){
-        this.parseLevel = parseLevel;
+     public Scope(int parseLvl){
+        this.parseLvl = parseLvl;
         hash = new Hashtable<>();
      }
      
      public Scope(){
-        parseLevel = 0;
+        parseLlv = 0;
      }
 
-     public void define(String name, Integer val){
+     public void addSymbol(String name, Integer val){
         hash.put(name, val);
      }
 
@@ -29,44 +29,105 @@ public class Scope {
         return  hash.get(name);
      }
     
-     public int getParseLevel(){
-        return parseLevel;
+     public int getParseLvl(){
+        return parseLvl;
      }
     }
 
     
-    
-    public class SymbolTable {
-    enter_scope()
-    exit_scope()
-    
+   
     }
 
-class symbolMethods {
+    class SymbolTable {
 
-    // private Stack<Scope> indexStructure;
+     private Stack<Scope> parseStructure;
+        private int currentLvl;
+        
 
+        /*
     public static void main(String args[]) {
 
     }
-
+*/
+    public SymbolTable(){
+       parseStructure = new Stack<>();
+       currentLvl = -1;
+    }
+        
     public void enterScope() {
+       currentLvl++;
+       parseStructure.push(new Scope(currentLvl));
 
     }
 
     public void exitScope() {
+       currentLvl--;
+       parseStructure.pop();
 
     }
 
-    public void addSymbol() {
-
+    public void addSymbol(String name, String val) {
+       Scope currentScope = parseStructure.peek();
+       currentScope.addSymbol(name, Integer.parseInt(val));
     }
 
-    public void findSymbol() {
-
+    public void findSymbol(String name) {
+        String result = "Error";
+        
+        if(parseStructure.isEmpty()){
+           return result;
+        }
+        
+        else{
+            /* for all parseStructures pS in Scope */
+           for(Scope pS:parseStructure){
+               if((currentLvl > pS.getParseLvl()) && pS.isMatching(name)){
+                   Integer val = pS.getParseVal(name);
+                   result = val.toString();
+               }
+           }
+       }
+       return result;
     }
 
-    public void checkSymbol() {
+    public void checkSymbol(String fileName) {
+        File file = new File(filename);
+        try {
+            Scanner in = new Scanner(file);
+            
+            while(in.hasNextLine()){
+                String line = in.nextLine();
+                String[] input = line.split(" ");
+                
+                if(input[0].equals("enterScope")){
+                    enterScope();
+                    System.out.println(line);
+                }
+
+                else if (input[0].equals("exitScope")){
+                    endScope();
+                    System.out.println(line);
+                }
+                
+                else if (input[0].equals("addSymbol")){
+                    String name = input[1];
+                    String val = input[2];
+                    addSymbol(name, val);
+                    System.out.println(line);
+                }
+                
+                else if(input[0].equals("use")){
+                    String name = input[1];
+                    System.out.println(line+" = " + findSymbol(name));
+                }
+                
+                else{
+                    System.out.println(line);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
