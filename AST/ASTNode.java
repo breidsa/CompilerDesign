7db4fc1fd47e3832a11c92a9baec62f1838f244d
breidsa <77807959+breidsa@users.noreply.github.com>
2022-04-12 18:45:59 +0100

    Update ASTNode.java

--- AST/ASTNode.java
+++ AST/ASTNode.java
@@ -1,14 +1,34 @@
-public class ASTNode {
-    // Object accept(Visitor v, ID id); /* This may have to be type ID...not sure */
+public abstract class ASTNode {
+    abstract Object accept(Visitor v); /* This may have to be type ID...not sure */
+    // might need children nodes
 }
 
-// 3 main classes for the nodes
-// statement class
-class ASTStmt extends ASTNode {
+// // 3 main classes for the nodes
+// // statement class
+// class ASTStmt extends ASTNode {
+
+// }
+
+// class Types extends ASTStmt{
+    
+// }
+
+// For return types ... relevant to line 81 in .y
+class EndFunction extends ASTNode{
+    
+    String exp;
+    
+    public EndFunction(String exp){
+        this.exp = exp;
+    }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
+    }
 
 }
 
-class ForLoop extends ASTStmt {
+class ForLoop extends ASTNode {
     
     String iterator;
     String conditional;
@@ -20,11 +40,16 @@ class ForLoop extends ASTStmt {
         this.conditional = conditional;
         this.increment = increment;
         this.loopBody = loopBody;
+    }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
+    }
     
 }
 
 // subclasses of statement
-class IfStmt extends ASTStmt {
+class IfStmt extends ASTNode {
     
     String ifStmt
     String thenStmt;
@@ -34,6 +59,10 @@ class IfStmt extends ASTStmt {
         this.ifStmt = ifStmt;
         this.thenStmt = thenStmt;
         this.elseStmt = elseStmt;
+    }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
     }
 
 }
@@ -46,6 +75,10 @@ class Asnmt extends ASTStmt {
         this.var = var;
         this.exp = exp;
     }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
+    }
 
 }
 
@@ -57,8 +90,30 @@ class VarDec extends ASTStmt {
         this.varType = varType;
         this.varName = varName;
     }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
+    }
 
 }
+   
+class StructCreator extends ASTStmt {
+    String name;
+    ArrayList<String> fieldTypes;
+
+    public StructCreator(String name, ArrayList<String> fieldTypes) {
+        this.name = name;
+        this.fieldTypes = fieldTypes;
+    }
+    
+    public Object accept(Visitor v) {
+	      return v.visit(this);
+    }
+}
+
+    
+    
+    
 
 // expression class
 class ASTExp extends ASTNode {
@@ -68,4 +123,44 @@ class ASTExp extends ASTNode {
 // typeclass
 class ASTType extends ASTNode {
 
+}
+    
+    
+    
+    
+public class AbstractVisitor implements Visitor{  
+
+	public Object visit(Var var){
+		/* var.getName().accept(this, id);
+    		var.getScope().accept(this, id);
+    		var.getType().accept(this, id); */
+		return null;
+	}
+	
+	public Object visit(Function func, ID id){
+		func.getName().accept(this, id);
+    		func.getScope().accept(this, id);
+    		if (func.getParamTypes() != null){
+        		for (String paramTyoes : Function.getParamTypes()) {
+				paramTypes.accept(this, id);
+		    	}
+    		}
+		return null;
+	}
+	
+	public Object visit(Struct struct, ID id){
+		/* struct.getName().accept(this, id);
+    		struct.getScope.accept(this, id); */
+    		struct.getFieldTypes().accept(this, id);
+		return null;
+	}
+}
+
+public interface Visitor {
+	
+	public Object visit(Var var);
+	
+	public Object visit(Function func);
+	
+	public Object visit(Struct struct);
 }
